d30a3fb696df878df29792821019536b3eac613c
Isabella Palacio Fajardo <150759038+IsitaP@users.noreply.github.com>
2025-08-26 22:07:43 -0500

    Update lanzador.pl

--- taller 1/lanzador.pl
+++ taller 1/lanzador.pl
@@ -8,21 +8,33 @@
 #     Fichero: script automatización ejecución por lotes 
 #****************************************************************/
 
-$Path = `pwd`;
-chomp($Path); 
+$Path = `pwd`; # Obtiene la ruta absoluta del directorio actual ejecutando `pwd` en la shell
+chomp($Path);  # quita el salto de línea final que deja `pwd`
 
+# Esta línea define el nombre del ejecutable a invoca
 $Nombre_Ejecutable = "mmClasicaOpenMP"; // en este se pone el nombre del ejecutable el cual se cambio por mmClasicaOpenMP
-@Size_Matriz = ("340","320","400","8000","9600","12000","4000","7200");
-@Num_Hilos = (1,2,4,8,16,20);
-$Repeticiones = 30;
+@Size_Matriz = ("340","320","400","8000","9600","12000","4000","7200");# Lista de tamaños de matriz (N). Cada N ejecutará N×N.
+@Num_Hilos = (1,2,4,8,16,20); # Lista de números de hilos a usar con OpenMP en cada corrida.
+$Repeticiones = 30;## Cantidad de repeticiones por combinación (tamaño, hilos) para obtener estadística.
 
+# Doble bucle: recorre cada tamaño y cada #hilos
 foreach $size (@Size_Matriz){
 	foreach $hilo (@Num_Hilos) {
+
+ 		# Nombre del archivo de salida donde se acumulan (append) los tiempos,
+        # un archivo por combinación N/hilos. Ej: mmClasicaOpenMP-340-Hilos-2.dat
 		$file = "$Path/$Nombre_Ejecutable-".$size."-Hilos-".$hilo.".dat";
-		for ($i=0; $i<$Repeticiones; $i++) {
+
+ # Ejecuta el programa $Repeticiones veces y agrega (>>) la salida al archivo.	
+  for ($i=0; $i<$Repeticiones; $i++) {
+
+  			 # Llama: /ruta/mmClasicaOpenMP <size> <hilos> >> archivo.dat
+            # La salida del programa (tiempo en µs) se anexa al .dat
 			system("$Path/$Nombre_Ejecutable $size $hilo  >> $file");
 			printf("$Path/$Nombre_Ejecutable $size $hilo \n");
 		}
+  
+        # Muestra en pantalla qué comando se ejecutó (progreso).
 		close($file);
 	$p=$p+1;
 	}
