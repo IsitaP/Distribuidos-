8d19dded893e3718060caf07bd08793fc6d0c81e
Isabella Palacio Fajardo <150759038+IsitaP@users.noreply.github.com>
2025-09-08 16:11:41 -0500

    Update socketudpser.java

--- taller socket Mailbox/socketudpser.java
+++ taller socket Mailbox/socketudpser.java
@@ -1,3 +1,10 @@
+/*#######################################################################################
+ # Fecha: 8-08-2025
+ # Autor: Isabella Palacio Fajardo, Ana Sofia Grass, Sergio Ortiz, Juan Sebastian Vargas
+ # Tema:
+ #   - Comunicación Cliente-Servidor en Java
+ #   - Implementación con Sockets TCP y UDP
+#######################################################################################*/
 import java.net.*;
 import java.io.*;
 // crea la clase socketudpser con su clase principal 
@@ -32,4 +39,5 @@ public class socketudpser {
       }
    }
 }
+
 
